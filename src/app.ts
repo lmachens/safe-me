96c7d872e66101b27e779de947684247d7040769
phlgr <philipp@gartz.dev>
2021-03-02 15:22:23 +0100

    🗃 Connect to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import { printWelcomeMessage, printNoAccess } from "./messages";
 import { askForAction, askForCredentials } from "./questions";
 import { handleGetPassword, handleSetPassword, hasAccess } from "./commands";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const run = async () => {
-  printWelcomeMessage();
+  const url = process.env.MONGODB_URL;
+
+  try {
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB!");
+    client.close();
+  } catch (error) {
+    console.error(error);
+  }
+  /*  printWelcomeMessage();
   const credentials = await askForCredentials();
   if (!hasAccess(credentials.masterPassword)) {
     printNoAccess();
@@ -19,7 +33,7 @@ const run = async () => {
     case "get":
       handleGetPassword(action.passwordName);
       break;
-  }
+  } */
 };
 
 run();
