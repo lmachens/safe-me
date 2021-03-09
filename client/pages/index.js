21e2615d85b5211b336dc1a612646002416f006c
fgottes <f.gottesmann@outlook.com>
2021-03-09 14:36:19 +0100

    Create Client for PasswordManager

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,31 @@
+import { useState } from "react";
+
 export default function Home() {
-  return <></>;
+  const [passwordName, setPasswordName] = useState("");
+  const [passwordDoc, setPasswordDoc] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const result = await fetch(
+      `http://localhost:3000/api/passwords/${passwordName}`
+    );
+    const passwordDoc = await result.json();
+    setPasswordDoc(passwordDoc);
+  }
+  return (
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          value={passwordName}
+          onChange={(event) => setPasswordName(event.target.value)}
+        />
+        <button type="submit">Send Request</button>
+      </form>
+      {passwordDoc && (
+        <>
+          {passwordDoc.name} {passwordDoc.value}
+        </>
+      )}
+    </>
+  );
 }
